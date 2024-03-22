1145efb6f9b8c1c987aa07fc4f92430e61c23c8d
roksolanamaksymovych <151615831+roksolanamaksymovych@users.noreply.github.com>
2024-03-22 09:15:21 +0200

    addlab2

---
+++ src/billboards_merge_sort.py
@@ -0,0 +1,49 @@
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+
+    return merge(left, right)
+
+
+def merge(left, right):
+    merged = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] >= right[j]:
+            merged.append(left[i])
+            i += 1
+        else:
+            merged.append(right[j])
+            j += 1
+    merged.extend(left[i:])
+    merged.extend(right[j:])
+    return merged
+
+
+def paint_billboards(k, t, lengths):
+    sorted_lengths = merge_sort(lengths)
+    painters = [0] * k
+
+    for length in sorted_lengths:
+        min_index = painters.index(min(painters))
+
+
+        if min_index > 0 and painters[min_index - 1] == 0:
+            min_index -= 1
+        elif min_index < k - 1 and painters[min_index + 1] == 0:
+            min_index += 1
+
+        painters[min_index] += length
+
+    return max(painters) * t
+
+
+k = 10
+t = 5
+lengths = [10, 5, 10, 15, 10, 5, 10, 15, 20, 20]
+
+print("Результат:", paint_billboards(k, t, lengths), "хвилин")
